085582e6fd307c1c009dee0615221db4d4a81d74
nitisha121 <nitisha.agarwal@uwaterloo.ca>
2019-01-09 23:35:58 -0500

    extra notes

---
+++ SQL/joins_and_more.sql
@@ -0,0 +1,11 @@
+/* add some notes on the following topics */
+
+1) left joins
+2) cross Joins
+3) inner Joins
+4) full outer joins
+5) concept of self joins to calculate differences
+6) details of group by + examples
+7) partitioning
+8) rank
+9) referencing other tables
